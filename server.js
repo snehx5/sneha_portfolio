51969bab13168716e5c6260cbac72e1bcd9f4d07
Sneha <sssnehshah24@gmail.com>
2018-06-09 22:36:01 -0400

    added express

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var router = express('router');
 
 app.use(express.static('public'));
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function() {
     console.log("App running on port : ", app.get('port'));
